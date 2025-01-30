a075625054f94bb3a3320278c66fe5f589f2b963
Amal <amal.nachit95@hotmail.com>
2025-01-30 11:16:15 +0100

    Creating the Repository MatchRepository

--- src/main/java/com/scorestable/restapi/Match.java
+++ src/main/java/com/scorestable/restapi/Match.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "Matches")  // Correspondance avec la table en base de données
+@Table(name = "Matches")
 public class Match {
 
     @Id
@@ -16,11 +16,9 @@ public class Match {
     private int scoreA;
     private int scoreB;
 
-    // Constructeur sans argument (obligatoire pour JPA)
     public Match() {
     }
 
-    // Constructeur avec arguments
     public Match(String teamA, String teamB, int scoreA, int scoreB) {
         this.teamA = teamA;
         this.teamB = teamB;
@@ -28,7 +26,6 @@ public class Match {
         this.scoreB = scoreB;
     }
 
-    // Getters et setters
     public Long getId() {
         return id;
     }
@@ -67,30 +64,5 @@ public class Match {
 
     public void setScoreB(int scoreB) {
         this.scoreB = scoreB;
-    }
-
-    // Méthodes utilitaires (facultatives)
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Match match = (Match) o;
-        return id != null && id.equals(match.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Match{" +
-                "id=" + id +
-                ", teamA='" + teamA + '\'' +
-                ", teamB='" + teamB + '\'' +
-                ", scoreA=" + scoreA +
-                ", scoreB=" + scoreB +
-                '}';
     }
 }
